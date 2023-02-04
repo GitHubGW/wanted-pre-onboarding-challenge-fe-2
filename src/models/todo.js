3f3ff12a71646264d4d680924fa6bb55c5cc2fe3
GitHubGW <kowonp@gmail.com>
2023-02-05 02:23:12 +0900

    feat: todo 모델 추가

---
+++ src/models/todo.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {Object} Todo
+ * @property {number} id - 아이디
+ * @property {string} content - 내용
+ * @property {boolean} isCompleted - 완료여부
+ * @property {string} category - 카테고리
+ * @property {string[]} [tags] - 태그들
+ */
+
+/**
+ * @type {Todo}
+ */
+const todo = {};
